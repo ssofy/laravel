069b6dcce3d705ff05a8c274739e5fb5f746198f
SSOfy <support@ssofy.com>
2023-06-18 16:40:39 +0300

    new Facade methods for various sso operations

--- src/Context.php
+++ src/Context.php
@@ -2,6 +2,9 @@
 
 namespace SSOfy\Laravel;
 
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Routing\Redirector;
 use SSOfy\APIClient;
 use SSOfy\APIConfig;
 use SSOfy\OAuth2Client;
@@ -9,12 +12,114 @@ use SSOfy\OAuth2Config;
 
 class Context
 {
+    const OAUTH2_WORKFLOW_STATE_SESSION_KEY = 'ssofy:oauth:workflow-state';
+
     /**
-     * @return UserTokenManager
+     * @var Session
      */
-    public function userTokenManager()
+    private $session;
+
+    public function __construct(Session $session)
     {
-        return app(UserTokenManager::class);
+        $this->session = $session;
+    }
+
+    /**
+     * @param string|null $redirectUri
+     * @return Application|\Illuminate\Foundation\Application|RedirectResponse|Redirector
+     */
+    public function initiateAuthorization($redirectUri = null)
+    {
+        $config = $this->defaultOAuth2Config();
+
+        $uri = $config->getAuthorizeUrl();
+
+        return $this->initAuthCodeFlow($uri, $redirectUri, $config);
+    }
+
+    /**
+     * @param string|null $token
+     * @param string|null $redirectUri
+     * @return Application|\Illuminate\Foundation\Application|RedirectResponse|Redirector
+     */
+    public function initiateAuthorizationWithToken($token = null, $redirectUri = null)
+    {
+        $config = $this->defaultOAuth2Config();
+
+        $uri = $config->getAuthorizeUrl($token);
+
+        return $this->initAuthCodeFlow($uri, $redirectUri, $config);
+    }
+
+    /**
+     * @param string $provider
+     * @param string|null $redirectUri
+     * @return Application|\Illuminate\Foundation\Application|RedirectResponse|Redirector
+     */
+    public function initiateSocialLogin($provider, $redirectUri = null)
+    {
+        $config = $this->defaultOAuth2Config();
+
+        $uri = $config->getSocialAuthorizeUrl($provider);
+
+        return $this->initAuthCodeFlow($uri, $redirectUri, $config);
+    }
+
+    /**
+     * @param string|null $redirectUri
+     * @return Application|\Illuminate\Foundation\Application|RedirectResponse|Redirector
+     */
+    public function register($redirectUri = null)
+    {
+        $config = $this->defaultOAuth2Config();
+
+        $uri = $config->getRegisterUrl();
+
+        return $this->initAuthCodeFlow($uri, $redirectUri, $config);
+    }
+
+    /**
+     * @param string|null $redirectUri
+     * @param bool $everywhere
+     * @return \Illuminate\Routing\Redirector|\Illuminate\Http\RedirectResponse
+     */
+    public function logout($redirectUri = null, $everywhere = false)
+    {
+        $this->logoutWithoutRedirect();
+
+        $config = $this->defaultOAuth2Config();
+
+        if (!is_null($redirectUri)) {
+            $config->setRedirectUri($redirectUri);
+        }
+
+        if ($everywhere) {
+            $url = $config->getLogoutEverywhereUrl();
+        } else {
+            $url = $config->getLogoutUrl();
+        }
+
+        return redirect($url);
+    }
+
+    /**
+     * @return void
+     */
+    public function logoutWithoutRedirect()
+    {
+        $ssoClient = $this->ssoClient();
+
+        $sessionState = $this->session->get(self::OAUTH2_WORKFLOW_STATE_SESSION_KEY);
+        if (empty($sessionState)) {
+            return;
+        }
+
+        $ssoClient->deleteState($sessionState);
+    }
+
+    public function account()
+    {
+        return redirect($this->ssoClient()->getAccountUrl());
     }
 
     /**
@@ -51,10 +156,10 @@ class Context
         $config = config('ssofy', []);
 
         return new APIConfig([
-            'domain'      => $config['domain'],
-            'key'         => $config['key'],
-            'secret'      => $config['secret'],
-            'secure'      => $config['secure'],
+            'domain'      => $config['api']['domain'],
+            'key'         => $config['api']['key'],
+            'secret'      => $config['api']['secret'],
+            'secure'      => $config['api']['secure'],
             'cache_store' => !empty($config['cache']['store']) ? app(Storage::class, [
                 'driver' => $config['cache']['store']
             ]) : null,
@@ -93,7 +198,15 @@ class Context
                 'driver' => $config['state']['store'],
             ]) : null,
             'state_ttl'         => $config['state']['ttl'],
-            'session_store'     => app(Session::class),
         ]);
+    }
+
+    private function initAuthCodeFlow($uri, $nextUri, $config)
+    {
+        $state = $this->ssoClient($config)->initAuthCodeFlow($uri, $nextUri);
+
+        $this->session->put(self::OAUTH2_WORKFLOW_STATE_SESSION_KEY, $state);
+
+        return redirect($uri);
     }
 }
--- src/Facades/SSOfy.php
+++ src/Facades/SSOfy.php
@@ -2,16 +2,23 @@
 
 namespace SSOfy\Laravel\Facades;
 
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Routing\Redirector;
 use Illuminate\Support\Facades\Facade;
 use SSOfy\APIConfig;
-use SSOfy\Laravel\UserTokenManager;
 use SSOfy\OAuth2Client;
 use SSOfy\OAuth2Config;
 
 /**
- * @method static UserTokenManager userTokenManager()
+ * @method static Redirector|RedirectResponse initiateAuthorization()
+ * @method static Redirector|RedirectResponse initiateAuthorizationWithToken()
+ * @method static Redirector|RedirectResponse initiateSocialLogin()
+ * @method static Redirector|RedirectResponse account()
+ * @method static Redirector|RedirectResponse register()
+ * @method static Redirector|RedirectResponse logout()
+ * @method static void logoutWithoutRedirect()
  * @method static OAuth2Client apiClient()
- * @method static OAuth2Client oauth2Client()
+ * @method static OAuth2Client ssoClient()
  * @method static APIConfig defaultAPIConfig()
  * @method static OAuth2Config defaultOAuth2Config()
  */
