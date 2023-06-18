b520ed88a6fee442ece35aa78804bdd15dba7407
SSOfy <support@ssofy.com>
2023-06-18 16:37:59 +0300

    new config structure

--- config/ssofy.php
+++ config/ssofy.php
@@ -6,13 +6,15 @@ return [
     | API Config
     |--------------------------------------------------------------------------
     |
-    | These credentials are required for the API connection.
+    | The following credentials are required for the API connection.
     |
     */
-    'domain'         => env('SSOFY_DOMAIN', 'us-1.api.ssofy.com'),
-    'key'            => env('SSOFY_KEY'),
-    'secret'         => env('SSOFY_SECRET'),
-    'secure'         => env('SSOFY_SECURE', true),
+    'api'    => [
+        'domain' => env('SSOFY_API_DOMAIN', 'us-1.api.ssofy.com'),
+        'key'    => env('SSOFY_API_KEY'),
+        'secret' => env('SSOFY_API_SECRET'),
+        'secure' => env('SSOFY_API_HTTPS', true),
+    ],
 
     /*
     |--------------------------------------------------------------------------
@@ -20,139 +22,33 @@ return [
     |--------------------------------------------------------------------------
     |
     | Caching can speed up response time by reducing the number of connections
-    | to SSOfy. Once a token is deleted, a signal will be emitted to this
-    | application to request cache invalidation for the deleted token.
+    | to SSOfy. Once a token is deleted, an event will be sent to this
+    | application which invalidates the deleted token.
     |
     */
-    'cache'          => [
+    'cache'  => [
         'store' => env('SSOFY_CACHE_DRIVER', null),
-        'ttl'   => env('SSOFY_CACHE_TTL', 10800), // time-to-live in seconds
+        'ttl'   => env('SSOFY_CACHE_TTL', 3600), // time-to-live in seconds
     ],
 
     /*
     |--------------------------------------------------------------------------
-    | OTP Settings
-    |--------------------------------------------------------------------------
-    |
-    | Set up a cache driver to be used for temporary token storage, including
-    | Action, OTP, and Auth tokens.
-    |
-    | Also, specify the Email and SMS channels to use when the user requests a
-    | new OTP.
-    |
-    */
-    'otp'            => [
-        'store'        => env('SSOFY_OTP_CACHE_DRIVER', 'file'),
-        'notification' => [
-            'brand' => env('APP_NAME'),
-
-            'email_channel' => env('SSOFY_OTP_EMAIL_CHANNEL', 'mail'),
-
-            // 'nexmo' in older laravel versions
-            'sms_chanel'    => env('SSOFY_OTP_SMS_CHANNEL', 'vonage'),
-        ],
-    ],
-
-    /*
-    |--------------------------------------------------------------------------
-    | Authentication Settings
-    |--------------------------------------------------------------------------
-    |
-    | Select which authentication methods to be allowed.
-    |
-    */
-    'authentication' => [
-        'username'     => true,
-        'email'        => true,
-        'phone'        => true,
-        'token'        => true,
-        'otp'          => true,
-        'passwordless' => true,
-    ],
-
-    /*
-    |--------------------------------------------------------------------------
-    | Repositories
-    |--------------------------------------------------------------------------
-    |
-    | You may extend the default repositories or replace with your own
-    | customized repositories to change how data is stored and retrieved
-    | from/to database or authentication providers.
-    |
-    */
-    'repository'     => [
-        'client' => \SSOfy\Laravel\Repositories\ClientRepository::class,
-        'scope'  => \SSOfy\Laravel\Repositories\ScopeRepository::class,
-        'user'   => \SSOfy\Laravel\Repositories\UserRepository::class,
-        'otp'    => \SSOfy\Laravel\Repositories\OTPRepository::class,
-        'api'    => \SSOfy\Laravel\Repositories\APIRepository::class,
-    ],
-
-    /*
-    |--------------------------------------------------------------------------
-    | User Data Settings
+    | SSOfy OAuth2 Client Settings
     |--------------------------------------------------------------------------
     */
-    'user'           => [
-        'model' => \App\Models\User::class, // Required only for "Registration" and "Password Reset" functionalities.
-
-        'filter' => \SSOfy\Laravel\Filters\UserFilter::class,
-
-        /**
-         * Specify the actual column name for each of the user claims.
-         * Set to null if no column exists in the database for the claim.
-         */
-        'column' => [
-            'id'                => 'id',
-            'hash'              => 'id',
-            'name'              => 'name',
-            'display_name'      => 'name',
-            'picture'           => 'avatar_url',
-            'username'          => 'username',
-            'email'             => 'email',
-            'email_verified_at' => 'email_verified_at',
-            'phone'             => 'phone',
-            'phone_verified_at' => 'phone_verified_at',
-            'password'          => 'password',
-            'metadata'          => 'metadata',
-        ],
-    ],
-
-    /*
-    |--------------------------------------------------------------------------
-    | Fixed Data
-    |--------------------------------------------------------------------------
-    |
-    | Default repositories generate a predefined set of scopes and clients,
-    | as specified below.
-    |
-    | For more complicated scenarios (e.g. read from database), you may need to
-    | extend the default repositories by overriding methods to meet your
-    | requirements.
-    |
-    | Read more in docs: https://www.ssofy.com/docs/SDK/Laravel/Repositories
-    |
-    */
-    'data'           => [
-        'scopes'  => [
-            '*' => [
-                'title'       => 'Read and Write all data.',
-                'description' => null,
-                'icon'        => 'fa-user-shield', // https://fontawesome.com
-                'url'         => null,
-            ]
-        ],
-        'clients' => [
-            'test' => [
-                'name'           => 'My App Name', // required
-                'secret'         => 'CLIENT SECRET KEY',
-                'redirect_uris'  => ['*'], // wildcard is supported but not recommended.
-                'icon'           => null,
-                'theme'          => 'default',
-                'tos'            => 'https://...',
-                'privacy_policy' => 'https://...',
-                'confidential'   => false, // https://oauth.net/2/client-types
-            ]
+    'oauth2' => [
+        'url'               => env('SSOFY_URL'),
+        'client_id'         => env('SSOFY_CLIENT_ID'),
+        'client_secret'     => env('SSOFY_CLIENT_SECRET'),
+        'redirect_uri'      => '/sso/callback',
+        'scopes'            => ['*'],
+        'locale'            => null, // 'en',
+        'timeout'           => 3600, // wait-for-login timeout in seconds
+        'pkce_method'       => 'S256', // options: plain, S256
+        'pkce_verification' => true,
+        'state'             => [
+            'store' => env('SSOFY_STATE_CACHE_DRIVER', 'file'),
+            'ttl'   => env('SSOFY_STATE_CACHE_TTL', 31536000), // time-to-live in seconds (default: 1-year)
         ],
     ],
 ];
